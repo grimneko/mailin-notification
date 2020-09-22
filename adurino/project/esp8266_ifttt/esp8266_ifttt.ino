59462fb298fa80148a54ce4ac99a2f1f7c3e4ae8
grimneko <github@grimneko.de>
2020-09-22 04:00:05 +0200

    added more documentation to esp8266_ifttt adurino code fixed a "invalid conversion from 'const char*' to 'char*'" condition in secrets_clean.h

--- adurino/project/esp8266_ifttt/esp8266_ifttt.ino
+++ adurino/project/esp8266_ifttt/esp8266_ifttt.ino
@@ -11,16 +11,16 @@
 
  
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); // establish serial connection for debugging
   Serial.setTimeout(2000);
 
   Serial.println();
   Serial.print("Connecting to access point... ");
-  WiFi.persistent(false);
+  WiFi.persistent(false); // for now dont save wifi data into flash to minimize flash memory overwrites
   
-  int i = 0;
+  int i = 0; // set connections attempt counter to 0
   while ((i < 2) && (WiFi.status() != WL_CONNECTED)) { // loop until we reached 3 attempts or WiFi.status() indicate we are connected
-    WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password); // start the connection attempt to user provided wireless lan
     Serial.print("Attempt ");
     Serial.print(i);
     Serial.print(" to connect to wifi network with SSID: ");
@@ -28,19 +28,19 @@ void setup() {
     delay(10000); // wait 10 sec to allow the esp to connect sucessful
     i++; // increment our attempt counter
   }
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println(" Couldn't establish WiFi connection after 3 attempts");
+  if (WiFi.status() != WL_CONNECTED) { // check if wifi is connected and react
+    Serial.println(" Couldn't establish WiFi connection after 3 attempts"); // we couldn't connect, thus just go back to sleep (to wake up again we reset anyway, ram gets cleaned)
     Serial.println("Going back to DeepSleep");
     ESP.deepSleep(0);
   }
-  send_webhook(eventname,apikey,"","","");
-  Serial.println(" Wifi connection established successful");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
   
+  Serial.println(" Wifi connection established successful"); // we are connected (and probably in the internet)
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP()); 
+  send_webhook(eventname,apikey,"","",""); // here is the magic happening, we notify ifttt we got an event (aka the mailbox got probably filled with something)
   Serial.println(" Disconnecting and going back to deep sleep");
-  WiFi.disconnect();
-  ESP.deepSleep(0);
+  WiFi.disconnect(); // we did send the event, now its time to sleep deep again thus disconnect ...
+  ESP.deepSleep(0); // and go back to sleep
 }
 
 void loop() {
--- adurino/project/esp8266_ifttt/secrets_clean.h
+++ adurino/project/esp8266_ifttt/secrets_clean.h
@@ -1,11 +1,11 @@
-#ifndef wlanpassword_h
-#define wlanpassword_h
+#ifndef secrets_h
+#define secrets_h
 
 // WiFi name and password
 const char *ssid = "wifiname"; //Enter your WIFI ssid
 const char *password = "wifipass"; //Enter your WIFI password
 
-const char *eventname = "mywebhookeventname"; //Enter the name of the IFTTTT event we want to trigger
-const char *apikey = "mywebhookkey"; //Enter the part after "https://maker.ifttt.com/use/" here as key
+char *eventname = "mywebhookeventname"; //Enter the name of the IFTTTT event we want to trigger
+char *apikey = "mywebhookkey"; //Enter the part after "https://maker.ifttt.com/use/" here as key
 
 #endif
