5535c4dfe5393a6fcc202c6a349952a4633316c4
grimneko <github@grimneko.de>
2020-09-22 02:59:45 +0200

    removed mailin-notification, added new folder structure for adurino code, added adurino lib as submodule

---
+++ adurino/project/esp8266_ifttt/esp8266_ifttt.ino
@@ -0,0 +1,44 @@
+/*
+ * mail-in notification via IFTTT
+ * Part of the Digital Prototype Academy course "Electronics + App Development" 
+ * Complete Project Details https://wikifactory.com/+dpa/project-iot
+ */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include "wlanpassword.h"
+ 
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
+
+  Serial.println();
+  Serial.print("Connecting to access point... ");
+  WiFi.persistent(false);
+  
+  int i = 0;
+  while ((i < 2) && (WiFi.status() != WL_CONNECTED)) { // loop until we reached 3 attempts or WiFi.status() indicate we are connected
+    WiFi.begin(ssid, password);
+    Serial.print("Attempt ");
+    Serial.print(i);
+    Serial.print(" to connect to wifi network with SSID: ");
+    Serial.println(ssid);
+    delay(10000); // wait 10 sec to allow the esp to connect sucessful
+    i++; // increment our attempt counter
+  }
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println(" Couldn't establish WiFi connection after 3 attempts");
+    Serial.println("Going back to DeepSleep");
+    ESP.deepSleep(0);
+  }
+
+  Serial.println(" Wifi connection established successful");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println(" Disconnecting and going back to deep sleep");
+  WiFi.disconnect();
+  ESP.deepSleep(0);
+}
+
+void loop() {
+}
